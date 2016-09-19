8d80c1dcbfb85c7754f5a9edf0ee9751ed494711
Febin George <fgeorge@numerix.com>
2016-09-19 13:13:37 -0400

    Final code with support for Market Cap and Avg Vol

--- pullfundamentals.py
+++ pullfundamentals.py
@@ -5,37 +5,48 @@ import timeit
 
 
 def getfundamentals(stockname):
-	return 0
 	url1 = 'https://finance.yahoo.com/quote/'
-	url2 = '/?p='
-	finalurl = url1+stockname+url2+stockname
+	midurl = '.BO'
+	url2 = '?p='
+	finalurl = url1+stockname+midurl+url2+stockname+midurl
 	print finalurl
 	htmlfile = urllib.urlopen(finalurl)
 	htmltext = htmlfile.read()
 	regex = '"trailingPE":{"raw":(.+?),"fmt"'
 	pattern = re.compile(regex)
 	price = re.findall(pattern,htmltext)
-	price2 = float(price[0])
-    regex2 = '$MARKET_CAP.1">(.+?)</td'
-    pattern = re.compile(regex2)
-    mcap = re.findall(pattern,htmltext)
-    mcap2 = mcap[0]
-    regex = 'averageVolume":{"raw":(.+?),"fmt"'
-    pattern = re.compile(regex)
-    avgvol  = re.findall(pattern,htmltext)
-    avgvol2 = float(price[0])
+	if price and price[0].find('Infinity') == -1:
+		price2 = float(price[0])
+	else:
+		price2 = 0
+	regex2 = '"marketCap":{"raw":(.+?),"fmt"'
+	pattern = re.compile(regex2)
+	mcap = re.findall(pattern,htmltext)
+	if mcap and mcap[0].find('Infinity') == -1:
+		mcap2 = float(mcap[0])
+	else:
+		mcap2 = 0
+	regex = 'averageVolume":{"raw":(.+?),"fmt"'
+	pattern = re.compile(regex)
+	avgvol  = re.findall(pattern,htmltext)
+	if avgvol and avgvol[0].find('Infinity') == -1:
+		avgvol2 = float(avgvol[0])
+	else:
+		avgvol2 = 0
 	return (price2,mcap2,avgvol2)
 
 if __name__ == '__main__':
 	csvfile = open('ListOfScrips.csv')
 	stockname = csv.reader(csvfile)
 	csvwriter = csv.writer(open("fundamentals.csv",'wb'))
+	header = ["stockname","PE","MCap","AvgVol"]
+	csvwriter.writerow(header)
 	for i,row in enumerate(stockname):
-        	if i == 3:
-			break
-        result = getfundamentals(row[1])
-		emptylist = [row[1],,result[1],result[2],result[3]]
+		result = getfundamentals(row[1])
+		emptylist = [row[1],result[0],result[1],result[2]]
 		csvwriter.writerow(emptylist)
+		
+	print "We are done here"
         
 	#tic=timeit.default_timer()
 	#pevalue= getfundamentals("ABB.BO")
